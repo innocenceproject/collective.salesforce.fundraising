621dceff0bf7644ad2a6606fc3710f1fb9e293b2
David Glick <dglick@gmail.com>
2012-05-07 09:47:00 -0700

    pep8/pyflakes cleanup

--- collective/salesforce/fundraising/fundraising_campaign.py
+++ collective/salesforce/fundraising/fundraising_campaign.py
@@ -4,26 +4,10 @@ from datetime import date
 from five import grok
 from plone.directives import dexterity, form
 
-from zope import schema
 from zope.interface import alsoProvides
 from zope.app.content.interfaces import IContentType
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
 
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
-from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
-
-from collective.salesforce.fundraising import MessageFactory as _
 
 
 # Interface class; used to define content-type schema.
@@ -32,7 +16,7 @@ class IFundraisingCampaign(form.Schema, IImageScaleTraversable):
     """
     A Fundraising Campaign linked to a Campaign in Salesforce.com
     """
-    
+
     form.model("models/fundraising_campaign.xml")
 
 alsoProvides(IFundraisingCampaign, IContentType)
@@ -100,13 +84,14 @@ class FundraisingCampaign(dexterity.Container):
 class CampaignView(grok.View):
     grok.context(IFundraisingCampaign)
     grok.require('zope2.View')
-    
+
     grok.name('view')
     grok.template('view')
 
     def addcommas(self, number):
         locale.setlocale(locale.LC_ALL, '')
         return locale.format('%d', number, 1)
+
 
 class CreatePersonalCampaignPageView(grok.View):
     grok.context(IFundraisingCampaign)
@@ -120,4 +105,3 @@ class CreatePersonalCampaignPageView(grok.View):
         from collective.salesforce.fundraising.forms import CreatePersonalCampaignPageForm
         form = CreatePersonalCampaignPageForm(self.request)
         return form
-
--- collective/salesforce/fundraising/personal_campaign_page.py
+++ collective/salesforce/fundraising/personal_campaign_page.py
@@ -1,30 +1,15 @@
+from datetime import date
 from five import grok
 from plone.directives import dexterity, form
 
-from zope import schema
 from zope.component import getUtility
 from zope.interface import alsoProvides
 from zope.app.content.interfaces import IContentType
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
 
 from Products.CMFCore.interfaces import ISiteRoot
 from Products.CMFCore.utils import getToolByName
 
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
-from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
-
-from collective.salesforce.fundraising import MessageFactory as _
 
 
 # Interface class; used to define content-type schema.
@@ -33,13 +18,13 @@ class IPersonalCampaignPage(form.Schema, IImageScaleTraversable):
     """
     A personal fundraising page
     """
-    
+
     # If you want a schema-defined interface, delete the form.model
     # line below and delete the matching file in the models sub-directory.
     # If you want a model-based interface, edit
     # models/personal_campaign_page.xml to define the content type
     # and add directives here as necessary.
-    
+
     form.model("models/personal_campaign_page.xml")
 alsoProvides(IPersonalCampaignPage, IContentType)
 
@@ -57,7 +42,7 @@ class PersonalCampaignPage(dexterity.Container):
             return None
         site = getUtility(ISiteRoot)
         pc = getToolByName(site, 'portal_catalog')
-        res = pc.searchResults(sf_object_id = self.parent_sf_id)
+        res = pc.searchResults(sf_object_id=self.parent_sf_id)
         if not res:
             return None
         return res[0].getObject()
@@ -122,8 +107,9 @@ class PersonalCampaignPage(dexterity.Container):
 # of this type by uncommenting the grok.name line below or by
 # changing the view class name and template filename to View / view.pt.
 
+
 class SampleView(grok.View):
     grok.context(IPersonalCampaignPage)
     grok.require('zope2.View')
-    
+
     # grok.name('view')
