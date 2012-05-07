bf859e5193784c3a80a7d7734115faf42a108c17
David Glick <dglick@gmail.com>
2012-05-07 15:19:27 -0700

    Make the create-personal-campaign-page view actually create personal campaigns (in both Plone and SF).
    Also get rid of the pointless (I think) wrapper of that form, and add a permission controlling who can add personal campaigns.

--- collective/salesforce/fundraising/forms.py
+++ collective/salesforce/fundraising/forms.py
@@ -1,7 +1,9 @@
 from five import grok
 from plone.directives import form
-
 from z3c.form import button, field
+from plone.dexterity.utils import createContentInContainer
+from Products.statusmessages.interfaces import IStatusMessage
+from Products.CMFCore.utils import getToolByName
 
 from collective.salesforce.fundraising.fundraising_campaign import IFundraisingCampaign
 from collective.salesforce.fundraising.personal_campaign_page import IPersonalCampaignPage
@@ -11,12 +13,12 @@ from collective.salesforce.fundraising import MessageFactory as _
 
 class CreatePersonalCampaignPageForm(form.Form):
     grok.name('create-personal-campaign-page')
-    grok.require('zope2.View')
+    grok.require('collective.salesforce.fundraising.AddPersonalCampaign')
     grok.context(IFundraisingCampaign)
 
     @property
     def fields(self):
-        return field.Fields(IPersonalCampaignPage).select('goal', 'personal_appeal')
+        return field.Fields(IPersonalCampaignPage).select('title', 'personal_appeal', 'goal')
 
     ignoreContext = True
 
@@ -26,11 +28,39 @@ class CreatePersonalCampaignPageForm(form.Form):
     @button.buttonAndHandler(_(u'Create'))
     def handleOk(self, action):
         data, errors = self.extractData()
-        import pdb; pdb.set_trace()
-
         if errors:
             self.status = self.formErrorsMessage
             return
+
+        # Add a personal campaign within the current context,
+        # using the data from the form.
+        parent_campaign = self.context
+        campaign = createContentInContainer(parent_campaign,
+            'collective.salesforce.fundraising.personalcampaignpage',
+            checkConstraints=False, **data)
+
+        # Add the campaign in Salesforce
+        # XXX Should include id of Contact for current user
+        # XXX Is the default status okay?
+        sfbc = getToolByName(self.context, 'portal_salesforcebaseconnector')
+        res = sfbc.create({
+            'type': 'Campaign',
+            'Type': 'Personal Fundraising',
+            'ParentId': parent_campaign.sf_object_id,
+            'Name': data['title'],
+            'Public_Name__c': data['title'],
+            'ExpectedRevenue': data['goal'],
+            })
+        if not res[0]['success']:
+            raise Exception(res[0]['errors'][0]['message'])
+
+        # Save the Id of the new campaign so it can be updated later.
+        campaign.sf_object_id = res[0]['id']
+        campaign.reindexObject(idxs=['sf_object_id'])
+
+        # Send the user to their new campaign.
+        IStatusMessage(self.request).add(u'Welcome to your personal campaign page!')
+        self.request.response.redirect(campaign.absolute_url())
 
     @button.buttonAndHandler(_(u"Cancel"))
     def handleCancel(self, action):
