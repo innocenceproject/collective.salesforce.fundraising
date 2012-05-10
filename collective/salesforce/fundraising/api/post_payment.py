7abf28ed7790533b6d67ac0c9dc2cb7c93e867d9
Jason Lantz <jlantz@innocenceproject.org>
2012-05-10 13:27:51 -0400

    Added in two changes that missed the last commit

--- collective/salesforce/fundraising/api/post_payment.py
+++ collective/salesforce/fundraising/api/post_payment.py
@@ -17,11 +17,12 @@ class PostPayment(BrowserView):
             return 'ERROR: No fundraising campaign was found with the id %s' % campaign_id
         campaign = res[0].getObject()
 
-        # Add the donation's amount to the campaign
+        # Add the donation's amount to the campaign and increment donations count
         if amount:
             # FIXME - This will error if non-numeric string passed as amount
             amount = int(amount)
             campaign.donations_total = campaign.donations_total + amount
+            campaign.donations_count = campaign.donations_count + 1
 
             # If this is a child campaign and its parent campaign is the parent
             # in Plone, add the value to the parent's donations_total
@@ -29,6 +30,7 @@ class PostPayment(BrowserView):
                 parent = campaign.aq_parent()
                 if parent.sf_id == campaign.parent_sf_id:
                     parent.donations_total = parent.donations_total + amount
+                    parent.donations_count = parent.donations_count + 1
 
         # Redirect the user to the campaign's thank you page
         self.request.RESPONSE.redirect(campaign.absolute_url() + '/@@thank-you')
