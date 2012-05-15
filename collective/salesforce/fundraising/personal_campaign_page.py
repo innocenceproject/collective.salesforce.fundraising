f99ecdd7461a9ee3d5376b8134a2bcd4ae36eba7
Jason Lantz <jlantz@innocenceproject.org>
2012-05-15 17:01:47 -0400

    Added Thank You page for Personal Campaign Page

--- collective/salesforce/fundraising/personal_campaign_page.py
+++ collective/salesforce/fundraising/personal_campaign_page.py
@@ -132,3 +132,43 @@ class SampleView(grok.View):
     grok.require('zope2.View')
 
     # grok.name('view')
+
+
+class ThankYouView(grok.View):
+    grok.context(IFundraisingCampaign)
+    grok.require('zope2.View')
+
+    grok.name('thank-you')
+    grok.template('thank-you')
+
+    def update(self):
+        # Fetch some values that should have been passed from the redirector
+        self.email = self.request.form.get('email', None)
+        self.first_name = self.request.form.get('first_name', None)
+        self.last_name = self.request.form.get('last_name', None)
+        self.amount = self.request.form.get('amount', None)
+
+        # Create a wrapped form for inline rendering
+        from collective.salesforce.fundraising.forms import CreateDonorQuote
+        if self.context.can_create_donor_quote():
+            self.donor_quote_form = CreateDonorQuote(self.context, self.request)
+            alsoProvides(self.donor_quote_form, IWrappedForm)
+
+        # Determine any sections that should be collapsed
+        self.hide = self.request.form.get('hide', [])
+        if self.hide:
+            self.hide = self.hide.split(',')
+
+    def render_janrain_share(self):
+        amount_str = ''
+        if self.amount:
+            amount_str = _(u' $%s' % self.amount)
+        comment = _(u'I just donated%s to a great cause.  You should join me.') % amount_str
+
+        return "rpxShareButton(jQuery('#share-message-thank-you'), 'Tell your friends you donated', '%s', '%s', '%s', '%s', '%s')" % (
+            self.context.description,
+            self.context.absolute_url(),
+            self.context.title,
+            comment,
+            self.context.absolute_url() + '/@@images/image',
+        )
