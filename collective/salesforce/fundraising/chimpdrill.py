264942823464d1c6ee4d1332417d94fbe026424a
Jason Lantz <jlantz@innocenceproject.org>
2013-03-07 15:48:17 -0500

    Added IPersonalPageCreated and IPersonalPageDonation schemas and added page_ fields in campaign data

--- collective/salesforce/fundraising/chimpdrill.py
+++ collective/salesforce/fundraising/chimpdrill.py
@@ -14,13 +14,58 @@ class IBaseCampaignEmail(Interface):
     )
     campaign_image_url = schema.TextLine(
         title=u"Campaign Image URL",
-        description=u"The URL of the main image for the campaign, if provided",
+        description=u"The URL of the full sized main image for the campaign, if provided",
         required=False,
     )
     campaign_header_image_url = schema.TextLine(
         title=u"Campaign Header Image URL",
-        description=u"The URL of the custom header image for the campaign, if provided",
+        description=u"The URL of the full sized custom header image for the campaign, if provided",
         required=False,
+    )
+    campaign_goal = schema.Int(
+        title=u"Campaign Goal",
+        description=u"The goal for the campaign",
+    )
+    campaign_raised = schema.Int(
+        title=u"Campaign Raised",
+        description=u"The amount raised thus far by the campaign",
+    )
+    campaign_percent = schema.Int(
+        title=u"Campaign Percent",
+        description=u"The percentage of goal completion by the campaign",
+    )
+
+    page_name = schema.TextLine(
+        title=u"Page Name",
+        description=u"If this donation was to a personal campaign page, the name of the page",
+    )
+    page_fundraiser_first = schema.TextLine(
+        title=u"Page Fundraiser First Name",
+        description=u"The first name of the fundraiser who created this page",
+    )
+    page_fundraiser_last = schema.TextLine(
+        title=u"Page Fundraiser Last Name",
+        description=u"The last name of the fundraiser who created this page",
+    )
+    page_url = schema.TextLine(
+        title=u"Page URL",
+        description=u"If this donation was to a personal campaign page, the url of the page",
+    )
+    page_image_url = schema.TextLine(
+        title=u"Page Image URL",
+        description=u"If this donation was to a personal campaign page, the url to the full size image for the pagej",
+    )
+    page_goal = schema.Int(
+        title=u"Page Goal",
+        description=u"If this donation was to a personal campaign page, the goal for the page",
+    )
+    page_raised = schema.Int(
+        title=u"Page Raised",
+        description=u"If this donation was to a personal campaign page, the amount raised thus far by the page",
+    )
+    page_percent = schema.Int(
+        title=u"Page Percent",
+        description=u"If this donation was to a personal campaign page, the percentage of goal completion by the page",
     )
 
 class IBaseHonoraryEmail(Interface):
@@ -85,3 +130,23 @@ class IThankYouEmail(form.Schema, ITemplate, IBaseCampaignEmail):
         description=u"The last name of the donor",
     )
 
+class PersonalPageCreated(form.Schema, ITemplate, IBaseCampaignEmail):
+    """ Schema for emails thanking fundraiser for creating a personal campaign page """
+
+class PersonalPageDonation(form.Schema, ITemplate, IBaseCampaignEmail):
+    amount = schema.Int(
+        title=u"Amount",
+        description=u"The amount of the donation",
+    )
+    donor_first_name = schema.TextLine(
+        title=u"Donor First Name",
+        description=u"The first name of the donor",
+    )
+    donor_last_name = schema.TextLine(
+        title=u"Donor Last Name",
+        description=u"The last name of the donor",
+    )
+    donor_email = schema.TextLine(
+        title=u"The Donor's email address",
+        description=u"The last name of the donor",
+    )
