a49ef0ad363f1fbf337c9de2a835ee9517745a97
Shane Hathaway <shane@hathawaymix.org>
2014-11-03 09:48:42 -0700

    Provide a stub of the Schema interface in plone.directives.form.schema if necessary, so objects can be deleted.  Refs #912.

--- collective/salesforce/fundraising/patch.py
+++ collective/salesforce/fundraising/patch.py
@@ -5,6 +5,7 @@ from zope.event import notify
 from Products.CMFCore.utils import getToolByName
 from plone.app.users.browser.register import RegistrationForm
 from collective.salesforce.fundraising.interfaces import MemberCreated
+from zope.interface import Interface
 
 orig_handle_join_success = RegistrationForm.handle_join_success
 
@@ -16,3 +17,19 @@ def handle_join_success(self, data):
     notify(MemberCreated(member))
     return res
 RegistrationForm.handle_join_success = handle_join_success
+
+
+# The plone.directives.form.schema.Schema interface is gone, but some
+# catalogs continue to depend on it.  Provide a stub so databases don't
+# break.  Refs #912.
+
+
+try:
+    from plone.directives.form.schema import Schema
+except ImportError:
+    from plone.directives.form import schema
+
+    class Schema(Interface):
+        """BBB stub"""
+
+    schema.Schema = Schema
